a11c69c4a994d455b6dc2aff9252a23153e7a309
Anselm Long <85387081+anselmlong@users.noreply.github.com>
2026-09-21 01:52:49 +0800

    Fix course discovery selection and unattended reporting

--- src/report_generator.py
+++ src/report_generator.py
@@ -59,9 +59,6 @@ class ReportGenerator:
         announcements_by_course = self._group_announcements_by_course(new_announcements)
         assignments_by_course = self._group_assignments_by_course(upcoming_assignments)
 
-        # Ignore this because we don't want to show new courses in the report
-        new_courses = []
-
         # Calculate totals
         total_size_bytes = sum(
             d.task.size_bytes for d in new_downloads + updated_downloads
